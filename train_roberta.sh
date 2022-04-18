5e377f28fb347845020357f702e184e8647f63da
Ahmedashorit <103510095+Ahmedashorit@users.noreply.github.com>
2022-04-18 18:56:51 +0300

    new update

--- train_roberta.sh
+++ train_roberta.sh
@@ -3,7 +3,7 @@ python run_qa.py \
   --dataset_name squad \
   --do_train \
   --do_eval \
-  --per_device_train_batch_size 12 \
+  --per_device_train_batch_size 6 \
   --learning_rate 3e-5 \
   --num_train_epochs 2 \
   --max_seq_length 384 \
