0f71b80525f6294cb61f178ea12ec946df45ff12
Ahmedashorit <103510095+Ahmedashorit@users.noreply.github.com>
2022-04-20 00:18:41 +0300

    change

--- train_deberta.sh
+++ train_deberta.sh
@@ -5,7 +5,7 @@ python run_qa.py \
   --do_eval \
   --per_device_train_batch_size 3 \
   --learning_rate 3e-5 \
-  --num_train_epochs 2 \
+  --num_train_epochs 1 \
   --max_seq_length 384 \
   --doc_stride 128 \
   --output_dir ./squad_deberta_base
